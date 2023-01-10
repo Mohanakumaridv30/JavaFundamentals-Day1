b3df02a70572524670a4a5f4ae3ffdcfd4a04aec
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 15:42:27 +0530

    Added : program to demonstrate methods

--- src/com/bridgelabz/javafundamentals/DemoMethod.java
+++ src/com/bridgelabz/javafundamentals/DemoMethod.java
@@ -1,2 +1,15 @@
-package com.bridgelabz.javafundamentals;public class DemoMethod {
+package com.bridgelabz.javafundamentals;
+
+public class DemoMethod {
+    public int addNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 15;
+        DemoMethod obj = new DemoMethod();
+        int result = obj.addNumbers(num1, num2);
+        System.out.println("Sum is: " + result);
+    }
 }
