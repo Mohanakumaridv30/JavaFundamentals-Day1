60b69e6bb983e378d19f5030b066a2b228e63516
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 15:38:43 +0530

    Added : The name using the command line

--- src/com/bridgelabz/javafundamentals/NameUsingCommandLine.java
+++ src/com/bridgelabz/javafundamentals/NameUsingCommandLine.java
@@ -1,2 +1,9 @@
-package com.bridgelabz.javafundamentals;public class NameUsingCommandLine {
+package com.bridgelabz.javafundamentals;
+
+public class NameUsingCommandLine {
+    public static void main(String[] args) {
+        for(String x: args) {
+            System.out.println(x);
+        }
+    }
 }
