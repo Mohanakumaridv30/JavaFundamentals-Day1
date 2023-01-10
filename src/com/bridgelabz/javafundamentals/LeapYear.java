1ef4787f0f0be42f2268f86b09c34eca009d80fe
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 15:43:28 +0530

    Added : Program to find Leap Year

--- src/com/bridgelabz/javafundamentals/LeapYear.java
+++ src/com/bridgelabz/javafundamentals/LeapYear.java
@@ -1,2 +1,23 @@
-package com.bridgelabz.javafundamentals;public class LeapYear {
+package com.bridgelabz.javafundamentals;
+
+import java.util.Scanner;
+public class LeapYear {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the Year : ");
+        Long year = s.nextLong();
+        if(year!=0)
+        {
+            if(year%400==0)
+                System.out.println(year+" is a leap year");
+            else  if(year%100==0)
+                System.out.println(year+" is not a leap year");
+            else if(year%4==0)
+                System.out.println(year+" is a leap year");
+            else
+                System.out.println(year+" is not a leap year");
+        }
+        else
+            System.out.println("Year zero does not exist ");
+    }
 }
