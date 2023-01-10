cd551f1b8ea7e45bfcb1cdc3e296a82c479362f9
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 15:10:09 +0530

    Added : Program to compare two strings

--- src/com/bridgelabz/javafundamentals/CompareTwoString.java
+++ src/com/bridgelabz/javafundamentals/CompareTwoString.java
@@ -1,2 +1,13 @@
-package com.bridgelabz.javafundamentals;public class CompareTwoString {
+package com.bridgelabz.javafundamentals;
+
+public class CompareTwoString {
+    public static void main(String[] args) {
+        String name1 = "Mohana";
+        String name2 = "Mohana";
+
+        if(name1 == name2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
 }
