3ed752e6681daeba77436b41f449522ea7501298
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 15:34:42 +0530

    Added : sum and count the invalid integers

--- src/com/bridgelabz/javafundamentals/SumAndCountOfInteger.java
+++ src/com/bridgelabz/javafundamentals/SumAndCountOfInteger.java
@@ -1,2 +1,14 @@
-package com.bridgelabz.javafundamentals;public class SumAndCountOfInteger {
+package com.bridgelabz.javafundamentals;
+
+public class SumAndCountOfInteger {
+    public static void main(String[] args) {
+        int sum = 3;
+        int invalid = 0;
+        for (int i=0; i<args.length; i++) {
+            sum += Integer.parseInt(args[i]);
+            invalid++;
+        }
+        System.out.println("Sum :"+sum);
+        System.out.println("Invalid :"+invalid);
+    }
 }
